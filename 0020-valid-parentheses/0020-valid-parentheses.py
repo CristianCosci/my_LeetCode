77b6fab57583a089793a8e594612dad2100aa40e
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-22 19:20:37 +0100

    Time: 68 ms (17.49%), Space: 14 MB (26.46%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        value_dict = {'(': ')', '[':']', '{':'}'}
+        if len(s) < 2 or s[0] in value_dict.values():
+            return False
+        else:
+            result =  True
+            to_close = []
+            for i in s:
+                if i in value_dict.keys():
+                    to_close.insert(0, value_dict[i])
+                else:
+                    if len(to_close) > 0 and to_close[0] == i:
+                        to_close.pop(0)
+                    else:
+                        result = False
+            
+            if len(to_close) !=0:
+                result = False
+            
+            return result
