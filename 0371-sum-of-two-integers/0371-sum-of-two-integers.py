6d5559c0012f6be035c151b8f6eef54d5173eb38
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-22 21:35:59 +0100

    Time: 47 ms (70.08%), Space: 13.9 MB (63.16%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -2,14 +2,14 @@ class Solution:
     def getSum(self, a: int, b: int) -> int:
         #convert into binary format to do bitshifting operation
         # https://www.youtube.com/watch?v=qq64FrA2UXQ&t=848s
-        mask = 0xffffffff #because in python number has infinite lenght, this is to avoid infinite loop cause by << 1
-        a = a & mask
+        #mask = 0xffffffff #because in python number has infinite lenght, this is to avoid infinite loop cause by << 1
+        a = a & 0xffffffff
         while b != 0:
-            sum = (a ^ b) & mask
-            b = ((a & b) << 1) & mask
+            sum = (a ^ b) & 0xffffffff
+            b = ((a & b) << 1) & 0xffffffff
             a = sum
             
         if (a>>31) & 1: # If a is negative in 32 bits sense
-            return ~(a^mask)
+            return ~(a^0xffffffff)
         
         return a
