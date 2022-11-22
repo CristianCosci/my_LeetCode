07b3505b925141062d4fe23a0f4564f6b9a9231d
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-22 19:30:40 +0100

    Time: 44 ms (90.93%), Space: 14 MB (99.54%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        mappatura = {}
+        for i in range(len(s)):
+            if s[i] not in mappatura and t[i] not in mappatura.values():
+                mappatura[s[i]] = t[i]
+            elif s[i] not in mappatura or mappatura[s[i]] != t[i]:  
+                return False
+        return True
