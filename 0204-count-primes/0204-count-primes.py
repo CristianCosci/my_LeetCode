d4190501a816a94a8052965edf4b8d260413d617
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-22 19:33:01 +0100

    Time: 9220 ms (6.27%), Space: 52.8 MB (84.44%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,18 @@
+'''
+    NB ---> see: Sieve of Eratosthenes method
+    link: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+'''
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <= 2:
+            return 0
+        else:
+            prime = [True] * n
+            prime[0] = prime[1] = False
+            i = 2
+            while i*i < n:
+                if prime[i] == True:  
+                    for j in range(i*i, n, i):
+                        prime[j] = False
+                i+=1
+            return sum(prime)
