65f76a2f76f16ac569dd18458dc6e9637cb1095f
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-23 16:50:40 +0100

    Time: 504 ms (8.02%), Space: 14.5 MB (6.28%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -17,20 +17,17 @@ class Solution:
                 elif i == len(flowerbed)-1:
                      if flowerbed[i] == 0:
                         if flowerbed[i-1] == 0:
-                            flowerbed[i] = 1
-                            n -= 1
+                            flowerbed, n = self.check(flowerbed, i, n)
                 else:
                     if flowerbed[i] == 0:
                         if flowerbed[i+1] == 0 and flowerbed[i-1] == 0:
-                            flowerbed[i] = 1
-                            n -= 1
+                            flowerbed, n = self.check(flowerbed, i, n)
                     
         elif len(flowerbed) == 1:
             if n == 0:
                     return True
                     
             if flowerbed[0] == 0:
-                flowerbed[0] = 1
-                n -= 1
+                flowerbed, n = self.check(flowerbed, 0, n)
         
         return n == 0
