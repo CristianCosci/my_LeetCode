029a25b3e09646158cf0a81c3e83b96f1a123108
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-26 22:26:34 +0100

    Time: 7522 ms (5.00%), Space: 18.3 MB (48.05%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -4,16 +4,23 @@ class KthLargest:
         self.k = k
         self.nums = sorted(nums, reverse=True)
         self.nums.append(-inf)
+        while len(self.nums) > self.k:
+            self.nums.pop()
 
     def add(self, val: int) -> int:
         i = 0
-        while True:
+        while i < len(self.nums):
             if val >= self.nums[i]:
                 self.nums.insert(i, val)
+                
+                if len(self.nums) > self.k:
+                    self.nums.pop()
+                
                 break
+                
             i += 1
             
-        return self.nums[self.k-1]
+        return self.nums[-1] if len(self.nums)>0 else None
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
