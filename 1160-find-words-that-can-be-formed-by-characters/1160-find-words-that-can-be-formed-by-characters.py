7463a5527933267010f5d18c3dadaf08f4907e30
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-27 11:32:39 +0100

    Time: 202 ms (82.39%), Space: 14.7 MB (5.74%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -4,12 +4,11 @@ class Solution:
         for word in words:
             aux = 0
             for char in word:
-                #print(word.count(char))
-                #print(char.count(char))
                 if word.count(char)>chars.count(char):
                     break
                 else:
                     aux += 1
+            
             if aux == len(word):
                 sum += aux
                 
