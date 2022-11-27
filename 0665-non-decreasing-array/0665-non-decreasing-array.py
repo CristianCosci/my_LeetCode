2b44b954bfda85bf23c3d33098a5c2432819ecd4
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-27 19:50:31 +0100

    Time: 200 ms (86.76%), Space: 15.3 MB (22.81%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.py
+++ 0665-non-decreasing-array/0665-non-decreasing-array.py
@@ -1,6 +1,7 @@
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
         err = 0
+        
         for i in range(1, len(nums)):
             if nums[i] < nums[i-1]:
                 if err or (i > 1 and i < len(nums)-1 and nums[i-2] > nums[i] and nums[i-1] > nums[i+1]):
