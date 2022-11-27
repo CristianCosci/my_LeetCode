bbb37d0940f1b501827e22f294be14b2222be389
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-27 11:34:22 +0100

    Time: 185 ms (87.00%), Space: 14.5 MB (85.81%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -3,7 +3,6 @@ class Solution:
         sum = 0
         for word in words:
             aux = True
-            
             for char in word:
                 if word.count(char)>chars.count(char):
                     aux = False
