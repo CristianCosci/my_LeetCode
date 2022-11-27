c6b1758911e64328cde3896b06cca9365f151e01
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-27 20:15:15 +0100

    Time: 48 ms (72.01%), Space: 13.9 MB (12.37%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def visit(self, node: Optional[TreeNode]) -> Optional[TreeNode]:
+    def visit(self, node: Optional[TreeNode]):
         if node is not None:
             tmp = node.left
             node.left = node.right
