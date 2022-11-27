d180efdbfc37b4f90c2f49d8570325900ed0db1f
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-27 15:02:08 +0100

    Time: 1664 ms (59.03%), Space: 46.6 MB (5.10%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_so_far = -inf
+        max_so_far = -1e5
         max_ending_here = 0
         for i in range(len(nums)):
             max_ending_here += nums[i]
