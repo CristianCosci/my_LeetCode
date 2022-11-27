a43c8008697c21ba7fa1e1baf33bd9eb78557fda
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-27 11:08:26 +0100

    Time: 779 ms (77.52%), Space: 27.7 MB (63.75%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -1,6 +1,7 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         lo, hi = 0, len(arr)-1
+        
         while lo < hi:
             m = (lo + hi) // 2
             if arr[m] < arr[m + 1]:
