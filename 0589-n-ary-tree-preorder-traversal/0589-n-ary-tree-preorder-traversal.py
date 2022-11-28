4959a731578d877f3b0dd811e840a918d56462b8
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-28 11:20:47 +0100

    Time: 118 ms (28.52%), Space: 16.3 MB (49.89%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def visit(self, root: 'Node', to_return):
+        if root is not None:
+            to_return.append(root.val)
+            for i in root.children:
+                self.visit(i, to_return)
+            
+            
+            
+    def preorder(self, root: 'Node') -> List[int]:
+        to_return = []
+        self.visit(root, to_return)
+        return to_return
