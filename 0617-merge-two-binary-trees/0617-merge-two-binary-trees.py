22203fde2c86326fe6c372b343596bfd147bfc37
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-28 09:54:03 +0100

    Time: 221 ms (16.43%), Space: 15.5 MB (82.04%) - LeetHub

---
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def visit(self, root1, root2):
+        if root1 == None and root2 == None:
+            return None
+        
+        if root1 is not None and root2 is not None:
+            root1.val = root1.val + root2.val
+            root1.left = self.visit(root1.left, root2.left)
+            root1.right = self.visit(root1.right, root2.right)
+            return root1
+        
+        if root1 is not None or root2 is not None:
+            return root1 or root2
+        
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        return self.visit(root1, root2)
