f2d9a62ffef91a92f301b4b65e98dd7433845848
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-29 11:06:40 +0100

    Time: 105 ms (52.11%), Space: 18.1 MB (39.99%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.py
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.py
@@ -5,17 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def visit(self, root, low, high):
-        if not root:
-            return None
-        elif root.val > high:
-            return self.visit(root.left, low, high)
-        elif root.val < low:
-            return self.visit(root.right, low, high)
-        else:
-            root.left = self.visit(root.left, low, high)
-            root.right = self.visit(root.right, low, high)
-            return root
+    
         
     def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
-        return self.visit(root, low, high)
+        def visit(root):
+            if not root:
+                return None
+            elif root.val > high:
+                return visit(root.left)
+            elif root.val < low:
+                return visit(root.right)
+            else:
+                root.left = visit(root.left)
+                root.right = visit(root.right)
+                return root
+        
+        return visit(root)
