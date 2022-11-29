6878db8d95c8a279f97aeaa1eb8c8ecb8146b633
Cristian <44636000+CristianCosci@users.noreply.github.com>
2022-11-29 14:57:36 +0100

    Time: 455 ms (15.68%), Space: 15.2 MB (92.52%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,10 +1,10 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        if sum(nums[1:]) == 0:
-            return 0
-        
         sum_left = 0
         sum_right = sum(nums) - nums[0]
+        if sum_right == 0:
+            return 0
+        
         for i in range(1, len(nums)-1):
             sum_left += nums[i-1]
             sum_right -= nums[i]
